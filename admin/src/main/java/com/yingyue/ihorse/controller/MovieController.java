8aa66d1a260ab295620ed9c39d385b695ac441b2
cuishiying163 <cuishiying163@163.com>
2017-06-14 00:19:26 +0800

    update

--- admin/src/main/java/com/yingyue/ihorse/controller/MovieController.java
+++ admin/src/main/java/com/yingyue/ihorse/controller/MovieController.java
@@ -1,7 +1,14 @@
 package com.yingyue.ihorse.controller;
 
+import com.yingyue.ihorse.entity.Admin;
+import com.yingyue.ihorse.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,6 +19,9 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/movie")
 public class MovieController {
 
+    @Autowired
+    private CustomerService customerService;
+
     /**
      * 信息发布
      * @return
@@ -19,6 +29,18 @@ public class MovieController {
     @RequestMapping(path = "/releaseMessage",method = RequestMethod.GET)
     public ModelAndView releaseMessageView(){
         ModelAndView model = new ModelAndView("release_message");
+        return model;
+    }
+    /**
+     * 报名管理
+     * @return
+     */
+    @RequestMapping(path = "/signupManage",method = RequestMethod.GET)
+    public ModelAndView signupManageView(@RequestParam(required = false) String keyword, @PageableDefault Pageable pageable){
+        ModelAndView model = new ModelAndView("signup_manage");
+        Page<Admin> page = customerService.getCustomers(pageable);
+        model.addObject("page",page);
+        model.addObject("keyword", keyword);
         return model;
     }
 }
