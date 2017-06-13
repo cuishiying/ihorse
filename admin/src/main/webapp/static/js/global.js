a47e79523af6d98a4cb5e987fa833d52ccfe1c3c
cuishiying163 <cuishiying163@163.com>
2017-06-13 10:39:28 +0800

    update

---
+++ admin/src/main/webapp/static/js/global.js
@@ -0,0 +1,14 @@
+/**
+ * Created by cuishiying on 2017/6/12.
+ */
+$(function () {
+    /*左侧导航开始*/
+    $(document).ready(function () {
+        $('ul.nav > li').click(function (e) {
+            e.preventDefault();
+            $('ul.nav > li').removeClass('active');
+            $(this).addClass('active');
+        });
+    });
+    /*左侧导航结束*/
+})
